46fb44cf6f1d456c516a2189eb82321e77f610a8
Tim Mullin <tmullin@gmail.com>
2009-06-20 08:46:09 +0000

    Added WT drum instrument groups and means for compatibility with RB.

--- src/jshm/Instrument.java
+++ src/jshm/Instrument.java
@@ -27,6 +27,7 @@ import jshm.util.Text;
 public enum Instrument {
 	GUITAR("gtr"),
 	BASS("bass"), // will also mean rhythm for GH
+	WTDRUMS(""),
 	DRUMS("drums"),
 	VOCALS("vocals");
 	
@@ -38,8 +39,9 @@ public enum Instrument {
 	}
 	
 	public javax.swing.ImageIcon getIcon() {
+		String s = WTDRUMS == this ? "DRUMS" : name();
 		return jshm.gui.GuiUtil.getIcon(
-			"instruments/" + name() + "_32.png");
+			"instruments/" + s + "_32.png");
 	}
 	
 	public String toShortString() {
@@ -47,6 +49,16 @@ public enum Instrument {
 	}
 	
 	public String getLongName() {
+		return getLongName(false);
+	}
+	
+	public String getLongName(boolean getWtVersion) {
+		if (getWtVersion) {
+			try {
+				return getText("longNameWt");
+			} catch (NullPointerException e) {}
+		}
+		
 		return getText("longName");
 	}
 	
@@ -60,7 +72,10 @@ public enum Instrument {
 	}
 	
 	public static Instrument smartValueOf(String value) {
-		String value2 = value.toUpperCase();
+		String value2 = value.toUpperCase().replace("_", "");
+		
+		if ("RBDRUMS".equals(value))
+			return DRUMS;
 		
 		for (Instrument i : values()) {
 			if (i.name().startsWith(value2))
@@ -71,33 +86,43 @@ public enum Instrument {
 	}
 	
 	public static enum Group {
-		GUITAR(1, Instrument.GUITAR),
-		BASS(2, Instrument.BASS),
-		DRUMS(3, Instrument.DRUMS),
-		VOCALS(4, Instrument.VOCALS),
+		GUITAR(1, 1, Instrument.GUITAR),
+		BASS(2, 2, Instrument.BASS),
+		WTDRUMS(0, 3, Instrument.WTDRUMS),
+		DRUMS(3, 4, Instrument.DRUMS),
+		VOCALS(4, 5, Instrument.VOCALS),
 		
-		GUITAR_BASS(5, Instrument.GUITAR, Instrument.BASS),
-		GUITAR_DRUMS(6, Instrument.GUITAR, Instrument.DRUMS),
-		GUITAR_VOCALS(7, Instrument.GUITAR, Instrument.VOCALS),
-		BASS_DRUMS(8, Instrument.BASS, Instrument.DRUMS),
-		BASS_VOCALS(9, Instrument.BASS, Instrument.VOCALS),
-		DRUMS_VOCALS(10, Instrument.DRUMS, Instrument.VOCALS),
+		GUITAR_BASS(5, 6, Instrument.GUITAR, Instrument.BASS),
+		GUITAR_WTDRUMS(0, 7, Instrument.GUITAR, Instrument.WTDRUMS),
+		GUITAR_DRUMS(6, 8, Instrument.GUITAR, Instrument.DRUMS),
+		GUITAR_VOCALS(7, 9, Instrument.GUITAR, Instrument.VOCALS),
+		BASS_WTDRUMS(0, 10, Instrument.BASS, Instrument.WTDRUMS),
+		BASS_DRUMS(8, 11, Instrument.BASS, Instrument.DRUMS),
+		BASS_VOCALS(9, 12, Instrument.BASS, Instrument.VOCALS),
+		WTDRUMS_VOCALS(0, 13, Instrument.WTDRUMS, Instrument.VOCALS),
+		DRUMS_VOCALS(10, 14, Instrument.DRUMS, Instrument.VOCALS),
 		
-		GUITAR_BASS_DRUMS(11, Instrument.GUITAR, Instrument.BASS, Instrument.DRUMS),
-		GUITAR_BASS_VOCALS(12, Instrument.GUITAR, Instrument.BASS, Instrument.VOCALS),
-		GUITAR_DRUMS_VOCALS(13, Instrument.GUITAR, Instrument.DRUMS, Instrument.VOCALS),
-		BASS_DRUMS_VOCALS(14, Instrument.BASS, Instrument.DRUMS, Instrument.VOCALS),
+		GUITAR_BASS_WTDRUMS(0, 15, Instrument.GUITAR, Instrument.BASS, Instrument.DRUMS),
+		GUITAR_BASS_DRUMS(11, 16, Instrument.GUITAR, Instrument.BASS, Instrument.DRUMS),
+		GUITAR_BASS_VOCALS(12, 17, Instrument.GUITAR, Instrument.BASS, Instrument.VOCALS),
+		GUITAR_WTDRUMS_VOCALS(0, 18, Instrument.GUITAR, Instrument.WTDRUMS, Instrument.VOCALS),
+		GUITAR_DRUMS_VOCALS(13, 19, Instrument.GUITAR, Instrument.DRUMS, Instrument.VOCALS),
+		BASS_WTDRUMS_VOCALS(0, 20, Instrument.BASS, Instrument.WTDRUMS, Instrument.VOCALS),
+		BASS_DRUMS_VOCALS(14, 21, Instrument.BASS, Instrument.DRUMS, Instrument.VOCALS),
 		
-		GUITAR_BASS_DRUMS_VOCALS(15, Instrument.GUITAR, Instrument.BASS, Instrument.DRUMS, Instrument.VOCALS)
+		GUITAR_BASS_WTDRUMS_VOCALS(0, 22, Instrument.GUITAR, Instrument.BASS, Instrument.WTDRUMS, Instrument.VOCALS),
+		GUITAR_BASS_DRUMS_VOCALS(15, 23, Instrument.GUITAR, Instrument.BASS, Instrument.DRUMS, Instrument.VOCALS)
 		
 		;
 		
 		public final int id;
+		public final int wtId;
 		public final int size;
 		public final Instrument[] instruments;
 		
-		private Group(final int id, final Instrument ... instruments) {
+		private Group(final int id, final int wtId, final Instrument ... instruments) {
 			this.id = id;
+			this.wtId = wtId;
 			this.size = instruments.length;
 			this.instruments = instruments;
 		}
@@ -107,12 +132,31 @@ public enum Instrument {
 			return Instrument.valueOf(this.name()).getIcon();
 		}
 		
-		
 		public String getLongName() {
+			return getLongName(false);
+		}
+		
+		public String getLongName(boolean getWtVersion) {
+			if (getWtVersion) {
+				try {
+					return getText("longNameWt");
+				} catch (NullPointerException e) {}
+			}
+			
 			return getText("longName");
 		}
 		
 		public String getWikiUrl() {
+			return getWikiUrl(false);
+		}
+		
+		public String getWikiUrl(boolean getWtVersion) {
+			if (getWtVersion) {
+				try {
+					return getText("wikiUrlWt");
+				} catch (NullPointerException e) {}
+			}
+			
 			return getText("wikiUrl");
 		}
 		
@@ -124,17 +168,22 @@ public enum Instrument {
 		}
 		
 		
-		/**
-		 * 
-		 * @param id
-		 * @return The group with id of <code>id</code>.
-		 * @throws IllegalArgumentException If no group exists for the given id.
-		 */
-		public static Group getById(final int id) {
-			for (Group g : Group.values())
-				if (g.id == id) return g;
-			
-			throw new IllegalArgumentException("invalid group id: " + id);
+		// not used anywhere
+//		/**
+//		 * 
+//		 * @param id
+//		 * @return The group with id of <code>id</code>.
+//		 * @throws IllegalArgumentException If no group exists for the given id.
+//		 */
+//		public static Group getById(final int id) {
+//			for (Group g : Group.values())
+//				if (g.id == id) return g;
+//			
+//			throw new IllegalArgumentException("invalid group id: " + id);
+//		}
+		
+		public static List<Group> getBySize(final int size) {
+			return getBySize(size, false);
 		}
 		
 		/**
@@ -143,14 +192,17 @@ public enum Instrument {
 		 * @return A List of Groups that have <code>size</code> number of instruments.
 		 * @throws IllegalArgumentException If <code>size</code> < 1.
 		 */
-		public static List<Group> getBySize(final int size) {
+		public static List<Group> getBySize(final int size, final boolean includeWtDrums) {
 			if (size < 1)
 				throw new IllegalArgumentException("size must be >= 1");
 			
 			List<Group> ret = new ArrayList<Group>();
 			
 			for (Group g : Group.values()) {
-				if (g.size == size) ret.add(g);
+				if (g.size == size) {
+					if (0 == g.id && !includeWtDrums) continue; // backward compat
+					ret.add(g);
+				}
 			}
 			
 			return ret;
